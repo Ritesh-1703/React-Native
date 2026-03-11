60427c29e06eefaddd46f837b25768fb57517c32
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-03-11 09:37:35 +0530

    FlexBox Practice Example

--- Component_App/src/Styling/InlineStyling.jsx
+++ Component_App/src/Styling/InlineStyling.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import FlexBoxLayout from '../FlexBoxLayout';
 const InlineStyling = () => {
   return (
     <View>
       <Text style={{ flex: 1, color: 'blue' }}>
         This is example on in line styling.
       </Text>
+      <FlexBoxLayout />
     </View>
   );
 };
