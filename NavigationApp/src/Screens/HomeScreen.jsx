551769a51227b1db99fdcdfc015e0e6f7469a41f
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-03-18 09:13:24 +0530

    Passing Parameters through Screens

--- NavigationApp/src/Screens/HomeScreen.jsx
+++ NavigationApp/src/Screens/HomeScreen.jsx
@@ -6,6 +6,10 @@ export default function HomeScreen({ navigation }) {
     <View>
       <Text>Home Screen</Text>
 
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile', { name: 'Ram', age: 25 })}
+      />
       <Button title="Open Drawer" onPress={() => navigation.openDrawer()} />
     </View>
   );
--- NavigationApp/src/Screens/ProfileScreen.jsx
+++ NavigationApp/src/Screens/ProfileScreen.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function ProfileScreen() {
+export default function ProfileScreen({ route }) {
+  const { name, age } = route.params;
   return (
     <View>
       <Text>Profile Screen</Text>
+      <Text>Name: {name}</Text>
+      <Text>Age: {age}</Text>
     </View>
   );
 }
